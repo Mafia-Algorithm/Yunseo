cebaa38a5479fe0a9a08989d5c552de333baca86
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-09-17 01:53:35 +0900

    [Programmers] 조이스틱

---
+++ Programmers/Level1/조이스틱.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+func solution(_ name:String) -> Int {
+    let name = name.map({$0})
+    var alphaCount = 0
+    for i in 0..<name.count {
+        if name[i] != "A" {
+            let up = name[i].asciiValue! - 65
+            let down = 91 - name[i].asciiValue!
+            alphaCount += Int((up<down) ? up : down)
+        }
+    }
+    
+    var minMove = name.count-1                  /// 좌우로 움직이는 최소 횟수
+    for i in 0..<name.count {
+        if name[i] != "A" {
+            var next = i + 1
+            while next<name.count && name[next] == "A" {
+                next += 1
+            }
+            let move = 2 *  i + name.count - next
+            minMove = min(move, minMove)
+        }
+    }
+    return alphaCount + minMove
+}
