ba054da142aed7d53c82949f17f59a961bbd8879
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-05-08 01:36:55 +0900

    [Baekjoon] 24502 - 알고리즘 수업 - 삽입 정렬 2

---
+++ Baekjoon/Bronze/24052.swift
@@ -0,0 +1,37 @@
+// 24052 - 알고리즘 수업 - 삽입 정렬2
+
+import Foundation
+
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let N = input[0], K = input[1]
+
+var arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+var count = 0
+for i in 1..<N {
+    var loc = i - 1
+    let newItem = arr[i]
+    
+    while loc >= 0 && arr[loc] > newItem {
+        arr[loc + 1] = arr[loc]
+        loc -= 1
+        count += 1
+        
+        if count == K {
+            print(arr.map{String($0)}.joined(separator: " "))
+            exit(0)
+        }
+    }
+    
+    if loc != i - 1 {
+        arr[loc + 1] = newItem
+        count += 1
+        
+        if count == K {
+            print(arr.map{String($0)}.joined(separator: " "))
+            exit(0)
+        }
+    }
+}
+
+print(-1)
